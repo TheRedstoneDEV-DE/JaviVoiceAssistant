5e5fc38bfadc7d9b39411983cb6582538773cd6e
TheDeveloper3 <thedeveloper.de@gmail.com>
2024-01-28 18:11:52 +0100

    Update to 1.4

---
+++ src/main/java/com/openai/OpenAPIConnector.java
@@ -0,0 +1,53 @@
+package com.openai;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class OpenAPIConnector {
+    public String infer(String prompt,String url, String model, String apiKey,String maxTokens){
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization", "Bearer " + apiKey);
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            // The request body
+            String body = "{\"max_tokens\": "+maxTokens+", \"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
+            connection.setDoOutput(true);
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(body);
+            writer.flush();
+            writer.close();
+
+            // Response from LLM
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+
+            StringBuffer response = new StringBuffer();
+
+            while ((line = br.readLine()) != null) {
+                response.append(line);
+            }
+            br.close();
+
+            // calls the method to extract the message.
+            return extractMessageFromJSONResponse(response.toString());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public String extractMessageFromJSONResponse(String response) {
+        int start = response.indexOf("content")+ 10;
+
+        int end = response.indexOf("\"", start);
+
+        return response.substring(start, end).replaceAll("\\\\n","\n");
+
+    }
+}
---
+++ src/main/java/voicecommands/VoicCommandAI.java
@@ -0,0 +1,20 @@
+package voicecommands;
+
+import general.CmdMask;
+import general.Main;
+import tts.TextToSpeech;
+
+public class VoicCommandAI extends CmdMask {
+    @Override
+    public void execute(String command, TextToSpeech tts, Main main) {
+        if(command.startsWith("start artificial intelligence")&&main.man.get("llm-enabled")!=null){
+            if (main.man.get("llm-enabled").contains("true")) {
+                tts.speak("switched to AI mode, to continue say any word");
+                main.va.aiMode = true;
+                main.va.isRunning = false;
+            }else{
+                tts.speak("AI mode is disabled, consult the manual");
+            }
+        }
+    }
+}
